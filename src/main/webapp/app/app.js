5c47ecfb0fb59418fa8142225d9051117488db10
Andre Martins <andrepm@gmail.com>
2015-01-29 15:59:59 +0100

    increase the polling interval to 15sec

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -3,6 +3,7 @@
 var CloudMessengerApp = angular.module("CloudMessengerApp", [
     "ui.bootstrap",
     "ngResource",
+    /*"ngWebSocket",*/
     "CloudMessengerApp.controllers",
     "CloudMessengerApp.services",
     "CloudMessengerApp.directives"
@@ -193,7 +194,7 @@ controllers.controller("MessengerCtrl", ['$scope', 'Message', 'Notification',
             });
         };
 
-        setInterval(refreshMessages, 5000);
+        setInterval(refreshMessages, 15000);
 
         refreshMessages();
   }]);
@@ -217,6 +218,28 @@ services.factory('AuthorizedSender', ['$resource',
         });
   }]);
 
+/*services.factory('XdiWs', ['$websocket',
+  function ($websocket) {
+        // Open a WebSocket connection
+        var dataStream = $websocket('wss://website.com/data');
+
+        var collection = [];
+
+        dataStream.onMessage(function (message) {
+            collection.push(JSON.parse(message.data));
+        });
+
+        var methods = {
+            collection: collection,
+            get: function () {
+                dataStream.send(JSON.stringify({
+                    action: 'get'
+                }));
+            }
+        };
+
+        return methods;
+  }]);*/
 
 services.factory('Notification', [
 
